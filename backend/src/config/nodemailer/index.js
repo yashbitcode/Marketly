fbd2ff7538ef28a81896316310149f9e25329636
YashBitCode <yashkumar.sre.177@gmail.com>
2026-07-20 22:33:54 +0530

    conn. updates

--- backend/src/config/nodemailer/index.js
+++ backend/src/config/nodemailer/index.js
@@ -1,9 +1,9 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST || "smtp.gmail.com",
-    port: Number(process.env.EMAIL_PORT || 587),
-    secure: false,
+     host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
